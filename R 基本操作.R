a07c4f80597455e6961d9f79622fac2f99dab8c7
play1029900 <play1029900@gmail.com>
2018-12-09 11:13:08 +0800

    ff

---
+++ R 基本操作.R
@@ -0,0 +1,50 @@
+prime = c(1,2,3,4,5)
+for (x in prime){
+  print(x)
+}
+
+y = c(5,2)
+y
+
+seq(2, 30,2)
+
+x <- c(1, 2, 3, 4)
+bseq <- x < 3
+bseq
+
+x <- 10
+str <- paste("x=", x)
+str
+
+labs <- paste(c("X", "Y"), 1:10, sep = "")
+labs
+
+x <- c(1, 2, NA,4)
+y <- x[!is.na(x)]
+y
+
+x <- c(0, -1, -2, NA, 4, 6)
+x <- x + 1
+z <- x[(!is.na(x)) & x > 0]
+z
+
+labs <- c("x", "y")[rep(c(1, 2, 2, 1),time = 4)]
+labs
+
+#====================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
